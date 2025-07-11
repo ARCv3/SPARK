c729ea10011d2ac187933f2e6ea70ab236e2aab5
israel.aristide <israel.aristide@plusgrade.com>
2025-07-11 01:58:05 -0400

    add modmail schema

---
+++ src/lib/arc/schema/v1/Modmail.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+import mongooseLong from 'mongoose-long'
+
+mongooseLong(mongoose);
+
+const { Types: { Long, ObjectId } } = mongoose;
+
+const ModmailSchema = new mongoose.Schema({
+    _id: ObjectId,
+    channelsnowflake: Long,
+    webhooksnowflake: Long,
+    usersnowflake: Long
+});
+
+const Modmail = mongoose.model("Modmail", ModmailSchema);
+
+export default Modmail;
