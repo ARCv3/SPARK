987725deadcd45edc182cbfc212aafd24e4bfcd3
israel.aristide <israel.aristide@plusgrade.com>
2025-10-13 20:26:20 -0400

    refac text keys and fix not found text keys

--- src/lib/hooks/useTextContent.ts
+++ src/lib/hooks/useTextContent.ts
@@ -18,15 +18,18 @@ export const useTextContent = (locale: Locale) => {
     const translations = translationFiles[locale];
 
     function text(code: keyof Translations, ...args: string[]): string {
-        
+
+        if (!(code in translations))
+            return code;
+
         let textContent = translations[code];
         const argsRegexp = /{\d}/g
         
         const results = textContent.match(argsRegexp);
 
-        results?.forEach(x => {
+        results?.forEach((x : string) => {
             const index = parseInt(x.replace(/[{}]/g, ''));
-            textContent = textContent.replace(x, args[index]);
+            textContent = textContent.replace(x, args[index].toString());
         });
 
         return textContent;
@@ -45,15 +48,37 @@ export const useTextContent = (locale: Locale) => {
 
 export interface Translations {
     
-    'arc.bot.name': string; // ARC
+    // Bot General
     'arc.blacklist': string; // "You are blacklisted from using that command!"
+    'arc.bot.name': string; // ARC
+    
+    // Commands
     'arc.command.setconfig.sucess': string; // Config has been sucessfully set
+    
+    // Modmail - General
     'arc.modmail.blacklisted': string; // "You are blacklisted from using modmail"
     'arc.modmail.channel.name': string; // Modmail
+    
+    // Modmail - Delivery
     'arc.modmail.delivery.emoji.delivered': string; // "📨"
     'arc.modmail.delivery.emoji.failed': string; // "🔴"
     'arc.modmail.delivery.recieved.description': string; // "Your modmail request was recieved! Please wait and a staff member will assist you shortly."
     'arc.modmail.delivery.recieved.footer': string; // v0.1 Thank you for using ARC
-    'arc.modmail.selectmenu.placeholder': string; // "Select a server to modmail: "
+    
+    // Modmail - Menu Buttons
+    'arc.modmail.menu.button.ban': string; // Ban
+    'arc.modmail.menu.button.ban.emoji': string; // 🔨
+    'arc.modmail.menu.button.ping': string; // Ping
+    'arc.modmail.menu.button.ping.emoji': string; // 📣
+    'arc.modmail.menu.button.save': string; // Save
+    'arc.modmail.menu.button.save.emoji': string; // 💾
+    
+    // Modmail - Menu General
+    'arc.modmail.menu.description': string; // A modmail session was opened with <@{0}>
+    'arc.modmail.menu.footer': string; // ARC v{0} - Modmail
+    'arc.modmail.menu.select.placeholder': string; // "Select a server to modmail: "
+    'arc.modmail.menu.title': string; // Modmail
+    
+    
 
 }
